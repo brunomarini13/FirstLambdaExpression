443d8d27a10017fc136ef1adc77d71e9a00364a4
Bruno <brunook13@outlook.com>
2021-09-12 10:04:52 -0300

    Add project files.

---
+++ StudyTest/Entities/Company.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyTest.Entities
+{
+    class Company
+    {
+        public string Name { get; set; }
+        public double MarketValue { get; set; }
+        public double LastAnnualProfit { get; set; }
+
+        public Company(string name, double marketValue, double lastAnnualProfit)
+        {
+            Name = name;
+            MarketValue = marketValue;
+            LastAnnualProfit = lastAnnualProfit;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Name);
+            sb.AppendLine("Market value: $" + MarketValue.ToString("f2", CultureInfo.InvariantCulture));
+            sb.AppendLine("Profits in last year: $" + LastAnnualProfit.ToString("f2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
---
+++ StudyTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using StudyTest.Entities;
+
+namespace StudyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Company> myList = new List<Company>();
+
+            Company a1 = new Company("Coca Cola", 8000000, 40000);
+            Company a2 = new Company("Pepsi", 6000000, 30000);
+            Company a3 = new Company("Fruki", 400000, 50000);
+            Company a4 = new Company("Dolly", 2000, 500);
+
+            myList.Add(a1);
+            myList.Add(a2);
+            myList.Add(a3);
+            myList.Add(a4);
+
+            /* sorting by Name */
+            myList.Sort((a1, a2) => a1.Name.ToUpper().CompareTo(a2.Name.ToUpper()));
+
+            /* sorting by MarketValue */
+             myList.Sort((a1, a2) => a1.MarketValue.CompareTo(a2.MarketValue));
+
+            /* sorting by Annual Profit */
+            myList.Sort((a1, a2) => a1.LastAnnualProfit.CompareTo(a2.LastAnnualProfit));
+
+            foreach (Company c in myList)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
